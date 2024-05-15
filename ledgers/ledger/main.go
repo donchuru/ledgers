7de85005251b15f7d3f02ce85d9c509f90940602
donchuru <onchuru@ualberta.ca>
2024-05-15 10:10:15 -0600

    config patch working

--- ledgers/ledger/main.go
+++ ledgers/ledger/main.go
@@ -5,7 +5,14 @@ import(
 	"os"
 	"time"
 	"strings"
+	"bufio"
 )
+
+func err_msg (e error) {
+	if e != nil {
+		fmt.Printf("Unable to write file: %v", e)
+	}
+}
 
 func main () {
 	/* take in command line arguments
@@ -16,13 +23,18 @@ func main () {
 	if len(os.Args) == 1 {
 		today := time.Now().Format("2006-01-02")
 		err := os.WriteFile(today, []byte(time.Now().Format("Monday, Jan 2, 2006") + "\n"), 0755)
-		if err != nil {
-			fmt.Printf("Unable to write file: %v", err)
-		}
+		err_msg(err)
 	} else if len(os.Args) > 2 {
-		err := os.WriteFile(strings.Join(os.Args[1:], " "), []byte(strings.Join(os.Args[1:], " ") + "\n" + time.Now().Format("Monday, Jan 2, 2006") + "\n\n"), 0755)
-		if err != nil {
-			fmt.Printf("Unable to write file: %v", err)
-		}
+		
+		f, _ := os.Open("../config/init.txt")
+		scanner := bufio.NewScanner(f)
+		scanner.Scan()
+		scanner.Scan()
+		location := scanner.Text()
+
+		// fmt.Println(location)
+
+		err := os.WriteFile(location + "\\" + strings.Join(os.Args[1:], " "), []byte(strings.Join(os.Args[1:], " ") + "\n" + time.Now().Format("Monday, Jan 2, 2006") + "\n\n"), 0755)
+		err_msg(err)
 	}
 }
